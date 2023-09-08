3361b6aa2325ebaadfd1cdfda28569e1d87f1221
Youmna Amin <youmnamin564@gmail.com>
2023-09-08 14:16:14 -0700

    ..

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry of the program
+ *
+ * Description: numbers sorting
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	for (int i = 97 ; i < 123 ; i++)
+		putchar(i);
+	return (0);
+}
