73be9114d30f688020a3fcb0571fc025dfcd0fff
Youmna Amin <youmnamin564@gmail.com>
2023-09-07 19:02:16 -0700

    ..

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+/**
+ * main - start of the program
+ *
+ * description - program prints certain line
+ *
+ * Return - zero
+*/
 
-main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
