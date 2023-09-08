91c387efdd932cc8bd7da2a550eee066a0cf07b5
Youmna Amin <youmnamin564@gmail.com>
2023-09-08 15:49:23 -0700

    ..

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry of the program
+ *
+ * Description: numbers sorting
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 103; i++)
+		putchar(i);
+	i = 10;
+	putchar(10);
+	return (0);
+}
