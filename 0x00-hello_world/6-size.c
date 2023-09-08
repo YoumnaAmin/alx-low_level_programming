029ec6acdd6ceab1b4ea0eb946b90ed1f155ec40
Youmna Amin <youmnamin564@gmail.com>
2023-09-07 19:35:34 -0700

    ..

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Description: A C program that prints with printf function
+ *
+ * Return: Always zero (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char"sizeof(char));
+	printf("Size of an int"sizeof(int));
+	printf("Size of a long int"sizeof(long int));
+	printf("Size of a long long int"sizeof(long long int));
+	printf("Size of a float"sizeof(float));
+	return (0);
+}
