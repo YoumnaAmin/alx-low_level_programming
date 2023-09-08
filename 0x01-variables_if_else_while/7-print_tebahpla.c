eb671c3c00856c44de007ad4acfc77c2dea80dd5
Youmna Amin <youmnamin564@gmail.com>
2023-09-08 15:39:25 -0700

    ..

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry of the program
+ *
+ * Description: numbers sorting
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 123; i > 96; i--)
+		putchar(i);
+	i = 10;
+	putchar(10);
+	return (0);
+}
