370eccfa8cbeb1fb71414f5a065d51adaf2ed86f
Youmna Amin <youmnamin564@gmail.com>
2023-09-08 15:24:50 -0700

    ..

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry of the program
+ *
+ * Description: numbers sorting
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	printf("\n");
+	return (0);
+}
