d7e0d6d526f8ab0c79fcba931833fa7370470eb6
Youmna Amin <youmnamin564@gmail.com>
2023-09-07 19:42:06 -0700

    ..

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	printf("Size of a char"sizeof(char));
-	printf("Size of an int"sizeof(int));
-	printf("Size of a long int"sizeof(long int));
-	printf("Size of a long long int"sizeof(long long int));
-	printf("Size of a float"sizeof(float));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
