c565aa8e48caf29f9b7343cb1cf6d20751fbe59c
Youmna Amin <youmnamin564@gmail.com>
2023-09-12 15:16:17 -0700

    ..

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - prints the time table of all numb
+*/
+
+void times_table(void)
+{
+	int i, j, mult;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(48);
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			mult = i * j;
+			if (j <= 9)
+				_putchar(' ');
+			else
+				_putchar((mult / 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+		_putchar('\n ');
+	}
+}
