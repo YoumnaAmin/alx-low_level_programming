12ffed1820700288d6d623042f706251b31851ec
Youmna Amin <youmnamin564@gmail.com>
2023-09-12 05:59:01 -0700

    ..

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry of program
+ *
+ * description: to print a sentence
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	int i;
+	char str[10] = "_putchar";
+
+	for (int i = 0; i < 10; i++)
+		putchar(str[i]);
+	putchar(10);
+	return (0);
+}
