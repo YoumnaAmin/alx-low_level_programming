5484487b990b7ca22579dd2f38a8c0c513db927a
Youmna Amin <youmnamin564@gmail.com>
2023-09-12 11:13:34 -0700

    ..

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - asolute for a number
+ *
+ * @n: character to calculate the abs
+ *
+ * description: computes the absolute value of an integer
+ *
+ * Return: zero
+*/
+
+int _abs(int n)
+{
+	abs(n);
+}
