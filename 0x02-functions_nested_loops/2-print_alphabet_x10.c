2140db4cdebf7b2ab11f67136785eaa8c2e1d5bb
Youmna Amin <youmnamin564@gmail.com>
2023-09-12 09:30:36 -0700

    ..

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry of program
+ *
+ * description: to print a sentence
+ *
+ * Return: zero
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (j = 0; j < 10; j++)
+		for (i = 97; i < 123; i++)
+			_putchar(i);
+	putchar(10);
+}
