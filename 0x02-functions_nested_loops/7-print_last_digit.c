b92b68efa67426846c161bbc01b85fed47df3953
Youmna Amin <youmnamin564@gmail.com>
2023-09-12 11:55:42 -0700

    ..

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit
+ *
+ * @n: character
+ *
+ * description: prints the last digit of a number
+ *
+ * Return: last digit
+*/
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	_putchar(n);
+}
