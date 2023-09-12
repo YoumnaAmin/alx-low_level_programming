17c4b6b2d2e66eb852daef0eed78807697bd5ce8
Youmna Amin <youmnamin564@gmail.com>
2023-09-12 15:28:49 -0700

    ..

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - addition
+ *
+ * @a: 1st I/N
+ *
+ * @b: 2nd I/O
+ *
+ * Return: summation
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
