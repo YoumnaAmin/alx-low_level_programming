cf8bd524003875ed40a697d9326d4774ac999899
Youmna Amin <youmnamin564@gmail.com>
2023-09-13 16:01:48 -0700

    ..

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main  - to print the sum of 3,6,9 multiples
+ *
+ * description: to print sum of 3, 6 and 9 multiples
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0 || i % 9 == 0)
+			sum += i;
+		i++;
+	}
+	printf("the sum is %d\n", sum);
+}
