6b1f5c697f906fa0d252f3b0de5891500977d35b
Youmna Amin <youmnamin564@gmail.com>
2023-09-14 17:27:48 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - fizzbuzz puzzle
+ *
+ * description: to print fizz if multiple 3, buzz for 5, fizzbuzz for both
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
