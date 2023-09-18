d171a9c11dad977f22b7ab1fda550653cadd31fb
Youmna Amin <youmnamin564@gmail.com>
2023-09-18 13:23:01 -0700

    ..

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - change the value of n 
+ * reset_to_98 - change the value of n
  *
- * @*n: point to n;
+ * @n: point to n;
 */
 
 void reset_to_98(int *n)
