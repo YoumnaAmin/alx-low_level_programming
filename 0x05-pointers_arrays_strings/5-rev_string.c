028fc741b1ffd5d624952093d4d3761ce7a2ab47
Youmna Amin <youmnamin564@gmail.com>
2023-09-18 14:57:53 -0700

    ..

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string -reveres string but doesn't print it
+ *
+ * @s: string
+*/
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+	char s2[len];
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		s2[(len - 1) - i] = s[i];
+	}
+	for (i = 0; i < len; i++)
+	{
+		s[i] = s2[i];
+	}
+}
