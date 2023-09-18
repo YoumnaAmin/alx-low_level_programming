2af6aaa6d44c961759e8ddb828c2a59fad4cb15c
Youmna Amin <youmnamin564@gmail.com>
2023-09-18 16:44:02 -0700

    ..

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,21 +11,11 @@ void puts_half(char *str)
 	int len = strlen(str);
 	int i, n;
 
-	if (len % 2 != 0)
+	len++;
+	n = len / 2;
+	for (i = n; i < len; i++)
 	{
-		n = (len - 1) / 2;
-		for (i = n; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else
-	{
-		n = len / 2;
-		for (i = n; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar(10);
 }
