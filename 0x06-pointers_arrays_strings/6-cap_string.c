7beb83851e1779a32c6977597c233a04402f4101
Youmna Amin <youmnamin564@gmail.com>
2023-09-21 17:48:19 -0700

    ..

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * islower - capitalizes all words of a string.
+ *
+ * @c: string
+ *
+ * Return: char
+*/
+
+int islower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * ischar - capitalizes all words of a string.
+ *
+ * @c: string
+ *
+ * Return: int
+*/
+
+int ischar(char c)
+{
+	int i;
+	char delim[] = "\t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delim[i])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @s: string
+ *
+ * Return: char
+*/
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int finddelim = 1;
+
+	while (*s)
+	{
+		if (ischar(*s))
+			finddelim = 1;
+		else if (islower(*s) && finddelim)
+		{
+			*s -= 32;
+			finddelim = 0;
+		}
+		else
+			finddelim = 0;
+		s++;
+	}
+	return (ptr);
+}
