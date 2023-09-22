6746c3d80a7964fb121c3d4c37beb887c1672069
Youmna Amin <youmnamin564@gmail.com>
2023-09-21 18:25:07 -0700

    ..

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ *
+ * @s: character
+ *
+ * Return: character
+*/
+
+char *leet(char *s)
+{
+	char *cp = c;
+
+	char key[] = {"A', 'E', 'O', 'T', 'L'};
+
+	int value[] = {4, 3, 0, 7, 1};
+
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+				*c = 48 + value[i];
+		}
+		c++;
+	}
+	return (cp);
+}
