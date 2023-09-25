a75e757b01d6f11c88a674514aca1766eed032f1
Youmna Amin <youmnamin564@gmail.com>
2023-09-25 14:10:15 -0700

    ..

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,16 +12,15 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (c == s[i])
 		{
-			p = &s[i - 1];
+			return (&s[i]);
 		}
 		else
 			continue;
 	}
-	return (p);
+	return (&s[i]);
 }
