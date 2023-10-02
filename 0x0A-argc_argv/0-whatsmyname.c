5bb6fa8129c6f3fbc4fb3acc8cc8b258ddbde89d
Youmna Amin <youmnamin564@gmail.com>
2023-10-02 14:15:54 -0700

    ..

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - to print the first command
+ *
+ * @argc: number of command word
+ * @argv: string in the command
+ *
+ * Return: return zero
+*/
+
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
