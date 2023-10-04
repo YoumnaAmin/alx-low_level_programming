7edba29150aa08b3741c9e6d720228d8f05f94a7
Youmna Amin <youmnamin564@gmail.com>
2023-10-04 12:04:20 -0700

    ..

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *ptr = (char *)malloc(size * sizeof(char));
+	char *ptr = (int *)malloc(size * sizeof(char));
 	unsigned int i;
 
 	if (size == 0)
