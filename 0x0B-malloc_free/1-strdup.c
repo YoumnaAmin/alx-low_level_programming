92a3c49d4f793dc07770631c775887139382268c
Youmna Amin <youmnamin564@gmail.com>
2023-10-04 14:37:04 -0700

    ..

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,9 +13,9 @@ char *_strdup(char *str)
 	int i, j;
 	char *ptr;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 1; str[i] != '\0'; i++)
 	{; }
-	ptr = malloc(i);
+	ptr = malloc(i * sizeof(char));
 	if (str == 0 || i == 0)
 	{
 		return (0);
