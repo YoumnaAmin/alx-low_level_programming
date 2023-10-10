7c38427fdf0abfcc42f336e6c918bbf09aaac80f
Youmna Amin <youmnamin564@gmail.com>
2023-10-10 13:48:00 -0700

    ..

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * init_dog -  initialize a variable of type struct dog
+ *
+ * @d: struct type
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: void(nothing)
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->age = age;
+	d->name = name;
+	d->owner = owner;
+}
