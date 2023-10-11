9c5a9bd6e37be8517ee350d04550a0db36a1e710
Youmna Amin <youmnamin564@gmail.com>
2023-10-11 14:56:31 -0700

    ..

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ *
+ * @name: name
+ * @f: function pointer
+ *
+ * Return: nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
