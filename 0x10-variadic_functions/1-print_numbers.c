ccb2d1a372c7bae0a26fb1a9d09fff113b5399cb
Youmna Amin <youmnamin564@gmail.com>
2023-10-12 08:18:24 -0700

    ..

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,5 +25,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n - 1; i++)
 		printf("%d%s", va_arg(args, int), separator);
 	printf("%d\n", va_arg(args, int));
-
+	va_end(args);
 }
