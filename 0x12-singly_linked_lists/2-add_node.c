f28bfd660659799fd7b91c04708c0858ffbc8cbb
YoumnaAmin <youmnamin564@gmail.com>
2023-10-19 16:01:24 -0700

    //

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - to add node at the begining
+ * @head: point to pointer
+ * @str: string
+ * Return: pointer const structure
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
