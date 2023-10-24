e62dec55c764701ab91664b58170f7b305b1269f
YoumnaAmin <youmnamin564@gmail.com>
2023-10-24 11:27:55 -0700

    ..

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 
 	i = 0;
-	if (head == NULL)
-		return (NULL);
 	while (i < index)
 	{
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 		i++;
 	}
 	if (head == NULL)
