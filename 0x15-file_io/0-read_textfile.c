4a1151b5fe558e178fa13b2be34e52c12a502eae
YoumnaAmin <youmnamin564@gmail.com>
2023-10-31 03:27:51 +0300

    ..

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,25 @@
 #include "main.h"
- 
+
  /**
-  * read_textfile - that reads a text file and prints it to the POSIX standard output
+  * read_textfile - that reads a text file and prints
   * @filename: the name of the file
   * @letter: unsigned int
   * Return: signed int
- */
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    int fd, check, checkw;
+	char *buf;
+	int fd, check, checkw;
 
-    buf = malloc(letters + 1);
-    if (filename == NULL)
-        return (0);
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        return (0);
-    }
-	check = read(fd, buf, letters);	
+	buf = malloc(letters + 1);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	check = read(fd, buf, letters);
 	if (check == -1)
 	{
 		return (0);
@@ -27,8 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf[letters] = '\0';
 	checkw = write(STDOUT_FILENO, buf, check);
 	if (checkw == -1)
-	    return (0);
-	
+		return (0);
 	close(fd);
 	return (checkw);
 }
