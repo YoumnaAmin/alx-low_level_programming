6ca62b9e5d27b6827af746fb4305d502ff0b005b
YoumnaAmin <youmnamin564@gmail.com>
2023-10-30 17:33:08 -0700

    ..

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
- /**
-  * read_textfile - that reads a text file and prints
-  * @filename: the name of the file
-  * @letter: unsigned int
-  * Return: signed int
-*/
+/**
+ * read_textfile -  reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ *
+ * Return: returns the actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
