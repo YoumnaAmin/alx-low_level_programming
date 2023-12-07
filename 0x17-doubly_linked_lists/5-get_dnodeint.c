5c68dcb205f018ab34d271495f1504480ebb5893
YoumnaAmin <youmnamin564@gmail.com>
2023-12-08 02:34:55 +0300

    ..

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,14 +6,12 @@
  * @index: the index number int
  * Return: pointer to node
 */
-size_t dlistint_len(const dlistint_t *h);
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
 	if (!head)
-		return (NULL);
-	if (index > (dlistint_len(head) - 1))
 		return (NULL);
 	i = 0;
 	while (head != NULL && i < index)
