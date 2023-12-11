c6dc772f1fc946702bdcbb7f0efa97bd8d2980ed
YoumnaAmin <youmnamin564@gmail.com>
2023-12-12 02:27:59 +0300

    ..

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/jackpot.so https://github.com/YoumnaAminwge/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
+wget -qO /tmp/jackpot.so https://github.com/YoumnaAmin/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
 export LD_PRELOAD=/tmp/jackpot.so
