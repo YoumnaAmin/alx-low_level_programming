cd67eda0d337518edeb70847b43b27dfdaaa3f76
YoumnaAmin <youmnamin564@gmail.com>
2023-12-23 00:41:32 +0300

    ..

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - to create a hash table
+ * @size: size of the tabele
+ * Return: struct of hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t*));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
