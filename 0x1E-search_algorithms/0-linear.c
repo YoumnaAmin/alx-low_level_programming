864839baf66c49f127e31596899606854bf65305
YoumnaAmin <youmnamin564@gmail.com>
2024-05-22 01:23:11 +0300

    ..

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i <= size; i++)
 	{
 		if (array == NULL || i == size)
-			return(-1);
+			return (-1);
 		if (array[i] == value)
 		{
 			printf("Value checked array[%d] = [%d]\n", i, array[i]);
