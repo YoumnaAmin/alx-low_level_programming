3aaea1e697518df963fb50583ff281eb589423d7
YoumnaAmin <youmnamin564@gmail.com>
2024-05-22 03:20:36 +0300

    ..

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "search_algos.h"
+
+/**
+ * print_array - Prints the current part of the array being searched
+ * @array: The array to be printed
+ * @left: The left index of the subarray
+ * @right: The right index of the subarray
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		if (i > left)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - Searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in the array
+ * @value: The value to search for
+ * Return: The index where the value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, mid;
+
+	if (array == NULL)
+		return (-1);
+
+	left = 0;
+	right = size - 1;
+	while (left <= right)
+	{
+		print_array(array, left, right);
+		mid = left + (right - left) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int BS(int *array, size_t size, size_t i, int value);
 
 #endif
